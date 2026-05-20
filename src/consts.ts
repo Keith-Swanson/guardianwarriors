f435c41ce100d67972a05f8bb7c531ee03b581cf
Keith Swanson <123766864+Keith-Swanson@users.noreply.github.com>
2026-05-20 11:52:10 -0400

    Fix squashed homepage by removing global main width constraint

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'Guardian Warriors';
+export const SITE_DESCRIPTION =
+	'Guardian Warriors protects people, systems, and communities through practical security guidance.';
