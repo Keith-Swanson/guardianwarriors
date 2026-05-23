fd4cf8383ebeeaecebea233b23edda599bd0e778
Keith Swanson <123766864+Keith-Swanson@users.noreply.github.com>
2026-05-23 00:30:46 -0400

    Resolve site issues: checks, CSS loading, mobile nav, and wrangler config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://guardianwarriors.org',
-	integrations: [mdx(), sitemap(), react(), tailwind()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
-	image: {
-		service: {
-			entrypoint: 'astro/assets/services/compile',
-		},
-	},
+  site: 'https://guardianwarriors.org',
+  adapter: cloudflare(),
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/compile'
+    }
+  }
 });
